8bee52a197c47219f2a6a8d627e4c7e72c32d8bb
raininmoonlight <kelbrito0912@gmail.com>
2024-11-25 21:15:55 -0300

    add

--- 007 Folder/game007.c
+++ 007 Folder/game007.c
@@ -1,1 +1,16 @@
 #include <stdio.h>
+
+int main() {
+    int dado = 1;
+    while (dado <= 6) {
+        if (dado < 6) {
+            printf("Sem YATZY!!\n");
+        } else {
+            printf("Yatzy\n");
+        }
+        printf("%d\n", dado);
+        dado = dado + 1;
+    }
+    
+    return 0;
+}
